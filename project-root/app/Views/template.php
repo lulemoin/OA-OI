dac181abaa187a586c2be8c79a6a33265995c73c
Lucas Le moine <lucas.le-moine@etu.utc.fr>
2020-10-14 07:08:04 +0400

    fix nav bar

--- project-root/app/Views/template.php
+++ project-root/app/Views/template.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,7 +10,7 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href='/OA-OI_git/project-root/public/data'>OA-OI</a>
+    <a class="navbar-brand" href='/OA-OI_git/project-root/public/Data'>OA-OI</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -26,11 +26,9 @@
             <li class="nav-item">
                 <a class="nav-link" href='/OA-OI_git/project-root/public/Dashboard'>what about me ?</a>
             </li>
+        </ul>
     </div>
 </nav>
-
-
-
 
 <?= $content ?>
 
